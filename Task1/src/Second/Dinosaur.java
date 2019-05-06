6376d37142d049a545821b635c0af3e97fe78095
anastasiakoroto <anastasiakoroto@gmail.com>
2019-05-06 11:04:46 +0300

    c:

---
+++ Task1/src/Second/Dinosaur.java
@@ -0,0 +1,32 @@
+package Second;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Dinosaur {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("Enter weight of dinosaur(kg): ");
+            double weightKg = scanner.nextDouble();
+
+            if (weightKg >= 0) {
+                double weightT = weightKg / 1000;
+                double weightG = weightKg * 1000;
+                double weightMg = weightG * 1000;
+
+                System.out.println("Weight in kilograms: " + weightKg + '\n'
+                        + "Weight in grams: " + weightG + '\n'
+                        + "Weight in milligrams: " + weightMg + '\n'
+                        + "Weight in tons: " + weightT);
+            } else {
+                System.out.println("No negative numbers. Choose another value.");
+            }
+
+        }
+        catch (InputMismatchException e) {
+            System.out.println("No symbols except comma and numbers. Try to enter a value again.");
+        }
+    }
+}
