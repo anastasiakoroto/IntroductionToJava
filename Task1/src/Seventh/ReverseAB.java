5430ff8b2c7a6cf97ebef8bc43f90c8f65cabd48
anastasiakoroto <anastasiakoroto@gmail.com>
2019-05-06 11:19:39 +0300

    c:

---
+++ Task1/src/Seventh/ReverseAB.java
@@ -0,0 +1,24 @@
+package Seventh;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ReverseAB {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter a, b:");
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            System.out.println("a: " + a + ", b: " + b);
+
+            a += b;
+            b = a - b;
+            a -= b;
+            System.out.println("New a: " + a + ", new b: " + b);
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Don't use any symbols. Integer numbers only.");
+        }
+    }
+}
